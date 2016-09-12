17f09b8d15b42634ad0f06ffb21f4c6460a17123
Daniel Blanco <daniel@blancomartin.cl>
2016-09-12 00:53:22 -0300

    [FIX] remove query of  nonexistent field

--- bmya_base_setup/company_image.py
+++ bmya_base_setup/company_image.py
@@ -13,8 +13,8 @@ class res_company(osv.osv):
     
     def init(self, cr):
         img=open(os.path.join(os.path.dirname(__file__), 'static', 'src', 'img','company_logo.png'), 'rb') .read().encode('base64')
-        cr.execute("UPDATE res_partner SET image=%s WHERE name = 'Your Company'", (img,))
-        cr.execute("UPDATE res_partner SET image=%s WHERE name = 'Blanco Martín EIRL'", (img,))
+        # cr.execute("UPDATE res_partner SET image=%s WHERE name = 'Your Company'", (img,))
+        # cr.execute("UPDATE res_partner SET image=%s WHERE name = 'Blanco Martín EIRL'", (img,))
         size = (180, None)
         cr.execute('UPDATE res_company SET logo_web=%s', (image_resize_image(img, size),))
 
