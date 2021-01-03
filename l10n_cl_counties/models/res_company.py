6192bf3c9a32f6d5ef9c3c63e47650fab51efe81
Daniel Blanco <daniel@blancomartin.cl>
2021-01-03 20:42:16 -0300

    [FIX] l10n_cl_counties: add different label to real_city

--- l10n_cl_counties/models/res_company.py
+++ l10n_cl_counties/models/res_company.py
@@ -4,5 +4,5 @@ from odoo import api, fields, models
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    real_city = fields.Char(related='partner_id.real_city', string='City')
+    real_city = fields.Char(related='partner_id.real_city', string='City.')
     city_id = fields.Many2one('res.city', related='partner_id.city_id', readonly=False)
