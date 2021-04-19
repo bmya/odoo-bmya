023b8b43f0eb22dccde7540963ae556e6eed567e
Daniel Blanco <daniel@blancomartin.cl>
2021-04-19 20:28:17 -0300

    [FIX] l10n_cl_edi_special_fields: change domain to include the possibility that the id could be the same as partner_id

--- l10n_cl_edi_special_fields/models/account_move.py
+++ l10n_cl_edi_special_fields/models/account_move.py
@@ -6,7 +6,8 @@ class AccountMove(models.Model):
 
     partner_invoice_id = fields.Many2one(
         'res.partner', string='Dirección de Facturación',
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id), ('parent_id', '=', partner_id)]", )
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id), "
+               "'|', ('parent_id', '=', partner_id), ('id', '=', partner_id)]", )
 
     @api.onchange('partner_id')
     def _onchange_partner(self):
