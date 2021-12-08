717c4110bbcd2931efcec8019ab67f4f12b92b44
Daniel Blanco <daniel@blancomartin.cl>
2021-12-08 19:31:01 -0300

    [FIX] l10n_latam_default_document: change order to asc

--- l10n_latam_default_document/models/account_move.py
+++ l10n_latam_default_document/models/account_move.py
@@ -10,4 +10,4 @@ class AccountMove(models.Model):
         self.l10n_latam_available_document_type_ids = False
         for rec in self.filtered(lambda x: x.journal_id and x.l10n_latam_use_documents and x.partner_id):
             rec.l10n_latam_available_document_type_ids = self.env['l10n_latam.document.type'].search(
-                rec._get_l10n_latam_documents_domain(), order='sequence desc')
+                rec._get_l10n_latam_documents_domain(), order='sequence asc')
