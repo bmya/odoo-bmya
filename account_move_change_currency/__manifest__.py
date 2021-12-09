a03c73715ef4666c881c3c3365278e251a34d09d
Daniel Blanco <daniel@blancomartin.cl>
2021-12-09 00:10:19 -0300

    [ADD] account_move_change_currency: add module [FIX] l10n_cl_counties: prevent singleton exception [FIX] l10n_cl_docsonline_partner: hide the docsonline button if does not applies

---
+++ account_move_change_currency/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'author': 'Blanco Martín & Asociados',
+    'category': 'Accounting & Finance',
+    'depends': ['account'],
+    'name': 'Account Move Change Currency',
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'wizard/account_change_currency_view.xml',
+        'views/move_view.xml',
+        'template/message_template.xml',
+    ],
+    'version': '2.0',
+    'website': 'https://www.bmya.cl',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+}
