e6f3616ae6d94664a1e226ce028fd9fc0f62a69b
Daniel Blanco <daniel@blancomartin.cl>
2022-08-03 16:48:40 -0400

    [ADD] l10n_cl_edi_qbli: this module provides a field in the XML of the electronic invoice named QBLI and required by some customers using SAP.

---
+++ l10n_cl_edi_qbli/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": """Chile - Add QBLI Field to invoices """,
+    'version': '1.0.',
+    'category': 'Localization/Chile',
+    'license': 'OPL-1',
+    'sequence': 12,
+    'author':  'Blanco Martín & Asociados',
+    'description': """
+Adds QBLI to the invoice lines, according to requirements from some customers using SAP
+=======================================================================================
+//CdgItem/TpoCodigo
+//CdgItem/VlrCodigo
+    """,
+    'website': 'http://bmya.cl',
+    'depends': [
+        'l10n_cl_edi',
+    ],
+    'data': [
+        'views/account_move_view.xml',
+        'template/dte_template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
