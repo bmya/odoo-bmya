29fe6b3a199d85cc82ab4a7d54731d2b21bf8142
augusto-weiss <awe@adhoc.com.ar>
2022-10-27 17:09:16 -0300

    [FIX] l10n_cl_counties: incompatible behaviour between differents localizations

--- l10n_cl_counties/models/res_partner.py
+++ l10n_cl_counties/models/res_partner.py
@@ -15,7 +15,7 @@ class ResPartner(models.Model):
         default=_default_country)
     state_id = fields.Many2one(
         "res.country.state", 'Ubication',
-        domain="[('country_id', '=', country_id), ('type', '=', 'normal'), ('id', '!=', id)]", readonly=True)
+        domain="[('country_id', '=', country_id), ('type', '=', 'normal'), ('id', '!=', id)]")
     real_city = fields.Char('City.')
 
     @api.onchange('city_id', 'city', 'state_id')
