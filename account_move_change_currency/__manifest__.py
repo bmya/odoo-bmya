7e13d5fd9ffa542eef80ed9e0648a55538c850e2
José Moreno Hanshing <jose@blancomartin.cl>
2022-12-12 10:23:20 -0300

    [FIX] account_move_change_currency: Fixed to work correctly on 15.0 Removed unnecessary rounding Simplified operations and avoided a divide by zero error You can now switch back and forth through currencies (eventually there is still a rounding difference after multiple operations)

--- account_move_change_currency/__manifest__.py
+++ account_move_change_currency/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'author': 'Blanco Martín & Asociados',
+    'author': 'Blanco Martín & Asociados, Adhoc S.A.',
     'category': 'Accounting & Finance',
     'depends': ['account'],
     'name': 'Account Move Change Currency',
@@ -10,7 +10,7 @@
         'views/move_view.xml',
         'template/message_template.xml',
     ],
-    'version': '2.0',
+    'version': '3.0',
     'website': 'https://www.bmya.cl',
     'license': 'LGPL-3',
     'installable': True,
