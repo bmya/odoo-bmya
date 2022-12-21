94ec2d589ce9358276b6f9ee1ccd0ab11f90438f
José Moreno Hanshing <jose@blancomartin.cl>
2022-12-20 21:20:00 -0300

    [MIG] l10n_cl_edi_special_fields: refactored module Split the partner extra fields and the invoice partner functionality The partner extra fields are now in l10n_cl_partner_extra_xml_identification

--- l10n_cl_edi_special_fields/__manifest__.py
+++ l10n_cl_edi_special_fields/__manifest__.py
@@ -1,22 +1,21 @@
 {
-    "name": """Chile - Special Fields """,
-    'version': '1.0.',
+    "name": """Chile - Legacy Special Fields """,
+    'version': '2.0.',
     'category': 'Localization/Chile',
     'license': 'OPL-1',
+    'icon': '/l10n_cl_partner_extra_xml_identification/static/description/icon.png',
     'sequence': 12,
     'author':  'Blanco Martín & Asociados',
     'description': """
-Agrega Campos Especiales a los XML
-==================================
-//Emisor/CdgVendedor
-//Receptor/CdgIntRecep
+    Permite emitir la factura a la sucursal de un cliente, y asignar la deuda directamente al cliente real.
+    Este módulo se conserva para mantener la compatibilidad con versiones anteriores, pero desde odoo 16 este problema
+    puede ser resuelto sin módulos adicionales.
     """,
     'website': 'http://blancomartin.cl',
     'depends': [
-        'l10n_cl_edi',
+        'l10n_cl_partner_extra_xml_identification',
     ],
     'data': [
-        'views/partner_view.xml',
         'views/account_move_view.xml',
         'template/dte_template.xml',
     ],
--- l10n_cl_edi_special_fields/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    l10n_cl_company_xml_identification = fields.Char('Company Seller Identification', help='CdgVendedor')
-    l10n_cl_partner_xml_identification = fields.Char('Customer Identification', help='CdgIntRecep')
