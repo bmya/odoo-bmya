93e13a29b97d3d3798f18834413376ddcfe9b303
hea-bmya <hector@blancomartin.cl>
2023-01-04 10:41:07 -0300

    [REF] Adapted to version 16.0

--- picking_from_xls/__manifest__.py
+++ picking_from_xls/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Picking from XLS File',
-    'author': u'Blanco Martín & Asociados',
+    'author': 'Blanco Martín & Asociados',
     'category': 'Inventory',
     'depends': ['stock'],
-    "external_dependencies": {
+    'external_dependencies': {
         'python': [
             'xlrd',
             'base64'
@@ -13,13 +13,11 @@
     'license': 'OPL-1',
     'price': 48.00,
     'currency': 'EUR',
-    'test': [],
     'data': [
         'views/stock_picking_view.xml',
         ],
-    'version': '11.0.1.0.0',
-    'website': 'http://blancomartin.cl',
-    'installable': False,
-    'auto-install': False,
-    'active': True
+    'version': '1.0',
+    'website': 'https://www.bmya.cl',
+    'installable': True,
+    'auto-install': False
 }
