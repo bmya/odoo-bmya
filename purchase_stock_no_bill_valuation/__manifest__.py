243bd5b520e35bec74b30e980a15f52e1a3d9c1d
José Moreno Hanshing <jose@blancomartin.cl>
2024-02-15 11:33:50 -0300

    [FIX]purchase_stock_no_bill_valuation: typos in manifest description

--- purchase_stock_no_bill_valuation/__manifest__.py
+++ purchase_stock_no_bill_valuation/__manifest__.py
@@ -1,11 +1,11 @@
 {
     'name': 'Disable Vendor Bill Valuation',
     'version': '1.0',
-    'summary': 'Disables the reevaluation of products when a vendor bill is posted with a different price than the one'
+    'summary': 'Disables the reevaluation of products when a vendor bill is posted with a different price than the one '
                'from the PO.',
-    'description': 'The module automatically disables the reevaluation in all of the product categories but can be'
-                   'configured with to work as before by setting "Reevaluate On Bill Price Difference" to True if you'
-                   'still wish to reevaluate certain product categories on'
+    'description': 'The module automatically disables the reevaluation in all of the product categories but can be '
+                   'configured with to work as before by setting "Reevaluate On Bill Price Difference" to True if you '
+                   'still wish to reevaluate certain product categories on '
                    'the vendor bills.',
     'category': 'Accounting',
     'author': 'Blanco Martín & Asociados',
