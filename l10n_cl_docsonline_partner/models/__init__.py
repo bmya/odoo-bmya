a61bdaeddbf3c013182c0438f4522f21478197d9
Daniel Blanco <daniel@blancomartin.cl>
2024-03-16 11:35:45 -0300

    [FIX] l10n_cl_docsonline_partner: Adapt module to Odoo 17 and add new section in configuration settings. [I18N] l10n_cl_docsonline_partner: add translations

--- l10n_cl_docsonline_partner/models/__init__.py
+++ l10n_cl_docsonline_partner/models/__init__.py
@@ -1,6 +1,2 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
 from . import partner
+from . import res_config_settings
---
+++ l10n_cl_docsonline_partner/models/res_config_settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import threading
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    docs_online_partner_token = fields.Char(
+        string="Documentos Online Token", config_parameter='docsonline.token')
+    docs_online_partner_replace_name = fields.Boolean(
+        string="Replace Name", config_parameter='docsonline.replace_name')
+    docs_online_partner_replace_street = fields.Boolean(
+        string="Replace Name", config_parameter='docsonline.replace_street')
+    docs_online_partner_replace_email = fields.Boolean(
+        string="Replace Name",config_parameter='docsonline.replace_email')
