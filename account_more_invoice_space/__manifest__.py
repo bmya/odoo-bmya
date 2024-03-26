bfc3390a266fa63ff6b092ba3f50abf11930bf4b
José Moreno Hanshing <jose@blancomartin.cl>
2024-03-25 22:45:44 -0300

    [ADD]account_more_invoice_space: reduce blank space below header and body's font size Change pt-5 on standard and boxed layouts, and mt-5 on the invoice PDF template to pt-0 and mt-0 respectively.
    Reduce font size to 80% on the invoice body

---
+++ account_more_invoice_space/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Blanco Martín & Asociados (https://www.bmya.cl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'More Invoice Space',
+    'summary': 'Reduces padding and margins below the header to the minimum, and reduces the font of the body to 80%.',
+    'version': '1.0.0',
+    'license': 'LGPL-3',
+    'author': 'Blanco Martín & Asociados',
+    'website': 'https://www.bmya.cl',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/report_invoice.xml',
+        'views/report_template.xml',
+    ],
+}
