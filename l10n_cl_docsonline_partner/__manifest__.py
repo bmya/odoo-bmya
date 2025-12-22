98533280a0fda7e0adfe799aa9cb34d4164325a4
Julia Elizondo <jue@adhoc.com.ar>
2025-12-22 18:43:05 +0000

    [FIX] l10n_cl_docsonline_partner_ fixed xpath on partner_view

--- l10n_cl_docsonline_partner/__manifest__.py
+++ l10n_cl_docsonline_partner/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": """Chile get customer data from www.documentosonline.cl""",
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.1.0',
     'category': 'Localization/Chile',
     'sequence': 12,
     'author':  'Blanco Martín & Asociados',
