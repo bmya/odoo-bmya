286ff0bb9be5dde1e2b525b097e03e2b64ac25aa
fw-bot-bmya <fw-bot-bmya@users.noreply.github.com>
2026-04-16 18:57:11 -0400

    [FW-BUMP] adaptar versiones 18.0→19.0
    - l10n_cl_edi_tpovta_fix: 18.0.1.0.0 → 19.0.1.0.0 (nuevo)

--- l10n_cl_edi_tpovta_fix/__manifest__.py
+++ l10n_cl_edi_tpovta_fix/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Chile - EDI TpoTranVenta Fix',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Localization/Chile',
     'license': 'LGPL-3',
     'icon': '/account/static/description/l10n.png',
