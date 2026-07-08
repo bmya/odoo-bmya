73987b003f7dc729408ce3dcc50379fed6fd4c36
Daniel Blanco <daniel@blancomartin.cl>
2026-07-07 20:19:59 -0400

    [ADD] l10n_cl_bank: account type for Chile and preferred payment journal for dashboard

---
+++ l10n_cl_bank/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Chile Localization Bank Account Types",
+    "version": "19.0.1.0.0",
+    "summary": "Agrega campos para determinar el tipo de cuenta y diario de pago preferido",
+    "author": "Blanco Martín y Asociados SpA",
+    'license': "LGPL-3",
+    "website": "https://www.bmya.cl",
+    "category": "Localization/Geopolitical Distribution",
+    "depends": [
+        "l10n_cl",
+        "base_address_extended",
+    ],
+    "data": [
+        "views/res_partner_bank_view.xml",
+    ],
+    "installable": True,
+}
